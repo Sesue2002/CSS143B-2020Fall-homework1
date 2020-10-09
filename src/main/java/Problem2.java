c9fe93ededd43a039e7669daaebe4cfba370f2e5
Saul <sjpaguada@gmail.com>
2020-10-08 23:52:32 -0700

    done

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -3,5 +3,18 @@ public class Problem2 {
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
         // Add your solution
+
+        int l = data.length;
+
+        for(int i = 0; i <  l - 1; i++){
+            for (int j = 0; j < l - i - 1; j++){
+                if (data[j] > data[j + 1])
+                {
+                    int tmp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = tmp;
+                }
+            }
+        }
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,7 +8,27 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
+
         // add your tests here
+
+        //test 1
+        int input[] = {3,2,1,5,6};
+        int answer[] = {1,2,3,5,6};
+
+        Problem2.bubbleSort(input);
+        assertArrayEquals(answer, input);
+
+        //test 2
+        int input1[] = {3};
+        int answer1[] = {3};
+
+        Problem2.bubbleSort(input1);
+        assertArrayEquals(answer1, input1);
+
+        //test 3
+        int input0[] = {0};
+
+        Problem2.bubbleSort(input0);
+
     }
 }
